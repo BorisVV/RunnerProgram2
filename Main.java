42f6acfa7352d6c935bbdcb5e2d2471701cf539b
Boris <bcarl@gmail.com>
2016-03-07 17:46:13 -0600

    added a method to ask the user.

--- Main.java
+++ Main.java
@@ -1,8 +1,6 @@
 package com.BorisV.java;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
 
@@ -12,35 +10,53 @@ public class Main {
         String lakeName;
         List<Double> allTimes = new ArrayList<>();
         List<String> lakes = new ArrayList<>();
-        Scanner in = new Scanner(System.in);
-        System.out.println("Enter the name of the Lake");
-        lakeName = in.nextLine();
-        System.out.println("How many time did you run around lake " + lakeName);
-        numOfTimes = in.nextInt();
-        times = new double[numOfTimes];
-        double min;
-        int counter = 0;
-        for (int i = 0; i < times.length; i++) {
-            counter++;
-            System.out.println("Enter the times " + counter);
-            times[i] = in.nextDouble();
+        Map<String, ArrayList<Double>> runner = new HashMap<>();
+
+        do {
+            Scanner in = new Scanner(System.in);
+            System.out.println("Enter the name of the Lake");
+            lakeName = in.nextLine();
+            System.out.println("How many time did you run around lake " + lakeName);
+            numOfTimes = in.nextInt();
+            times = new double[numOfTimes];
+            double min;
+            int counter = 0;
+            for (int i = 0; i < times.length; i++) {
+                counter++;
+                System.out.println("Enter the times #" + counter);
+                times[i] = in.nextDouble();
                 if (lakes.contains(lakeName)) {
                     allTimes.add(times[i]);
+                    runner.put(lakeName, (ArrayList<Double>)allTimes);
                 } else {
                     lakes.add(lakeName);
                     allTimes.add(times[i]);
                 }
 
-        }
-        min = times[0];
-        for (double t : times) {
-            if (min > t) min = t;
-            System.out.println(lakeName + " " + t);
-        }
+            }
+            min = times[0];
+            for (double t : times) {
+                if (min > t) min = t;
+                System.out.println(lakeName + " " + t);
+            }
 
-        System.out.println("The best time for lake: \n" + lakeName + " " + min);
+            System.out.println("The best time for lake: \n" + lakeName + " " + min);
+
+
+        } while (doYouWantToCont());
 
 
     }
+
+    public static boolean doYouWantToCont() {
+        Scanner in = new Scanner(System.in);
+        String y;
+        System.out.println("\nWould you like to add another lake? (y/n)");
+        y = in.next();
+        if (y.equalsIgnoreCase("Y"))
+            return true;
+        else return false;
+    }
 }
+
 
